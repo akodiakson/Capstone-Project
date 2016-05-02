3e16cf3d53539839b1815322e02b6c657b1c3cf8
Andrew Erickson <andrewericksonisu@gmail.com>
2016-05-01 19:25:26 -0500

    -- Display the current balls, strikes and pitches on the widget

--- app/src/main/java/com/akodiakson/pitchcounter/widget/GameEntryWidget.java
+++ app/src/main/java/com/akodiakson/pitchcounter/widget/GameEntryWidget.java
@@ -2,10 +2,19 @@ package com.akodiakson.pitchcounter.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.widget.RemoteViews;
 
 import com.akodiakson.pitchcounter.R;
+import com.akodiakson.pitchcounter.data.GameContentProvider;
+import com.akodiakson.pitchcounter.data.GameContract;
+import com.akodiakson.pitchcounter.data.GameCursorUtil;
+import com.akodiakson.pitchcounter.model.Game;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,10 +24,32 @@ public class GameEntryWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        Date today = new Date();
+        String gameDate = new SimpleDateFormat("yyyyMMdd").format(today);
+
+        ContentResolver contentResolver = context.getContentResolver();
+        Cursor cursor = contentResolver.query(
+                GameContentProvider.CONTENT_URI,
+                GameContract.PROJECTION_ALL_COLUMNS,
+                GameContract.DATE + " = ?", //selection
+                new String[]{gameDate}, null);
+
+        Game game;
+        if(cursor != null){
+            cursor.moveToFirst();
+            game = GameCursorUtil.buildGame(cursor);
+        } else {
+            game = new Game();
+        }
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.game_entry_widget);
-        views.setTextViewText(R.id.total_pitch_count, "42");
+        views.setOnClickPendingIntent(R.id.game_entry_ball_button, null);
+        views.setOnClickPendingIntent(R.id.game_entry_strike_button, null);
+        views.setTextViewText(R.id.total_pitch_count, String.valueOf(game.getPitches()));
+        views.setTextViewText(R.id.game_entry_ball_button_value, String.valueOf(game.getBalls()));
+        views.setTextViewText(R.id.game_entry_strike_button_value, String.valueOf(game.getStrikes()));
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
